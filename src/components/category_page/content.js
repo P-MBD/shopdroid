91ff57ca661017ac59677a4053b61df61a6d46fc
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-05-17 18:15:14 +0330

    47_new_category_2

--- src/components/category_page/content.js
+++ src/components/category_page/content.js
@@ -1,20 +1,49 @@
 import React from 'react'
-import {View, Text, Image, StyleSheet, TouchableOpacity,Dimensions} from 'react-native'
+import {View, Text, Image, StyleSheet, FlatList,Dimensions,TouchableOpacity} from 'react-native'
+import {cat_item} from '../../data/dataArray'
 const w = Dimensions.get('screen').width;
 const Content = () => {
     return(
         <View style={{flex:1}}>
-            <TouchableOpacity style={styles.btn}>
+            <FlatList 
+                data={cat_item}
+                renderItem={({item,key})=>
+                    <TouchableOpacity style={styles.btn}>
+                        <Image 
+                         style={styles.img}
+                         source={{uri:item.img}} />
+                        <Text  style={styles.txt}>
+                            {item.name}
+                        </Text>
+                    </TouchableOpacity>
+                }
+                keyExtractor={item=>item.id}
+             
 
-            </TouchableOpacity>
+            />
         </View>
     )
 }
 const styles = StyleSheet.create({
         btn:{
             width:w,
-            height:w/11,
-            backgroundColor:'gray'
+            height:w/7,
+            borderBottomWidth:.4,
+            borderColor:'gray',
+            flexDirection:'row',
+            paddingRight:15,
+            paddingLeft:15,
+            justifyContent:'space-between',
+            alignItems:'center'
+        },
+        img:{
+            width:w/6,
+            height:'90%',
+            resizeMode:'contain'
+        },
+        txt:{
+            fontSize:18,
+            color:'#555',
         }
 })
 

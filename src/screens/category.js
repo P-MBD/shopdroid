8cf9a8d3c44b9fbe10f43aa50dfad874c491a148
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-05-17 19:01:02 +0330

    48_new_category_3

--- src/screens/category.js
+++ src/screens/category.js
@@ -1,37 +1,25 @@
 import React, { Component } from 'react'
 import { Container, Header, Tab, Tabs, ScrollableTab } from 'native-base'
 import Content from '../components/category_page/content';
+import { cat_list } from '../data/dataArray';
 const Category=(prpos)=>{ 
     return (
       <Container>
-     <Tabs  renderTabBar={()=> <ScrollableTab />}>
-          <Tab heading="Tab1">
-              <Content />
-          </Tab>
-          <Tab heading="Tab2">
-            
-          </Tab>
-          <Tab heading="Tab3">
-          
-          </Tab>
-          <Tab heading="Tab1">
-           
-           </Tab>
-           <Tab heading="Tab2">
-             
-           </Tab>
-           <Tab heading="Tab3">
-           
-           </Tab>
-           <Tab heading="Tab1">
-           
-           </Tab>
-           <Tab heading="Tab2">
-             
-           </Tab>
-           <Tab heading="Tab3">
-           
-           </Tab>
+     <Tabs  renderTabBar={()=> <ScrollableTab  backgroundColor='#EF394E'/>}>
+        {
+           cat_list.map((item,key)=>(
+            <Tab 
+                heading={item.name}
+                tabStyle={{backgroundColor:'#EF394E'}}
+                activeTabStyle={{backgroundColor:'#EF394E'}}
+                textStyle={{color:'#fafafa',fontSize:14}}
+                activeTextStyle={{color:'#fff'}}
+            >
+                <Content />
+            </Tab>
+        ))
+
+        }
         </Tabs>
       </Container>
         );

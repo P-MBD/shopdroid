f0f2fac5219d834e841095cb169b204ddbfe7329
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-13 08:11:32 +0330

    73_add_comment_page_2

---
+++ src/components/add_component_page/send_co.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Text, View, TextInput, StyleSheet} from 'react-native'
+
+const Send_Co =()=>{
+    return(
+        <View>
+
+        </View>
+    )
+}
+const style= StyleSheet.create({
+
+})
+
+export default React.memo(Send_Co)
--- src/screens/add_comment.js
+++ src/screens/add_comment.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react'
-import { Container, Header, Tab, Tabs, ScrollableTab } from 'native-base'
+import {View,StyleSheet,Text} from 'react-native'
+import { Container, Header, Tab, Tabs, ScrollableTab, Content} from 'native-base'
 import My_Header from '../components/header/my_header'
 import My_Footer from '../components/footer/my_footer'
-
-
+import Send_Co from '../components/add_component_page/send_co'
+import Rait_sq from '../components/common/rait_sq'
 const props= {
     head_name:'Cat', 
     name_page: 'نظرات کاربران',
 }
-
 const Add_Comment =()=>{ 
     return (
-      <Container>
+      <Container style={{backgroundColor:'#ddd'}}>
         <My_Header {...props} /> 
+         <Content style={{padding:5}}>
+             <Text style={styles.reting_text}>امتیاز شما به این محصول</Text>
+             <View style={styles.reting_view}>
+                    <Rait_sq />
+            </View> 
+            <Send_Co />
+         </Content>
         <My_Footer name={'ثبت نقد و برسی'}/>
 
       </Container>
         );
      }
 
+  const styles = StyleSheet.create({
+    reting_text:{
+      fontSize:13,
+      fontFamily:'IRANSansMobile_Light',
+      color:'#666',
+      marginTop:10,
+      marginBottom:15,
+    },
+    reting_view:{
+      backgroundColor:'#fff',
+      padding:5,
+      marginRight:5,
+      marginLeft:5,
+    }
+  })
 export default React.memo(Add_Comment)
